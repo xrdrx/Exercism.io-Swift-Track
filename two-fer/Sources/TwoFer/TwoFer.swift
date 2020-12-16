046c864793ab2170cb3dcd9b360104f72de6b28b
xrdrx <ryd3rr@gmail.com>
2020-12-16 21:58:27 +0300

    Two Fer update

--- two-fer/Sources/TwoFer/TwoFer.swift
+++ two-fer/Sources/TwoFer/TwoFer.swift
@@ -1,6 +1,5 @@
 //Solution goes in Sources
-class TwoFer {
-    static func twoFer(name: String = "you") -> String {
-        return "One for \(name), one for me."
-    }
+func twoFer(name: String = "you") -> String {
+    return "One for \(name), one for me."
 }
+
