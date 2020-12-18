6b9f2a81ef4edab942d20cacae3e14df1f8f68d6
xrdrx <ryd3rr@gmail.com>
2020-12-18 16:43:24 +0300

    Leap update

--- leap/Sources/Leap/Leap.swift
+++ leap/Sources/Leap/Leap.swift
@@ -3,14 +3,10 @@ class Year {
     private let year: Int
     
     var isLeapYear: Bool {
-        return yearIsLeapYear()
+        return year.isMultiple(of: 4) && (!year.isMultiple(of: 100) || year.isMultiple(of: 400))
     }
     
     init(calendarYear: Int) {
         self.year = calendarYear
-    }
-    
-    private func yearIsLeapYear() -> Bool {
-        return year.isMultiple(of: 4) && (!year.isMultiple(of: 100) || year.isMultiple(of: 400))
     }
 }
