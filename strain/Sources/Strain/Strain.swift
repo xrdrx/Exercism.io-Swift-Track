2a6794e5f213232b37ee7f9dc5d29e18bcc06385
xrdrx <ryd3rr@gmail.com>
2020-12-21 17:02:33 +0300

    Strain

---
+++ strain/Sources/Strain/Strain.swift
@@ -0,0 +1,18 @@
+//Solution goes in Sources
+extension Collection {
+    func keep(_ function: (Element) -> Bool) -> [Element] {
+        var result: [Element] = []
+        for value in self {
+            if function(value) { result.append(value) }
+        }
+        return result
+    }
+    
+    func discard(_ function: (Element) -> Bool) -> [Element] {
+        var result: [Element] = []
+        for value in self {
+            if !function(value) { result.append(value) }
+        }
+        return result
+    }
+}
