d0c2f6dddad7d8761cdbf883cd45366b7789249f
xrdrx <ryd3rr@gmail.com>
2020-12-27 00:16:12 +0300

    Series

---
+++ series/Sources/Series/Series.swift
@@ -0,0 +1,17 @@
+//Solution goes in Sources
+struct Series {
+    private let serie: String
+    
+    init(_ serie: String) {
+        self.serie = serie
+    }
+    
+    func slices(_ length: Int) -> [[Int]] {
+        return getValues(length, serie)
+    }
+    
+    private func getValues(_ length: Int, _ line: String) -> [[Int]] {
+        if line.count < length { return [[]] }
+        return [line.prefix(length).compactMap { Int(String($0)) }] + getValues(length, String(line.dropFirst()))
+    }
+}
