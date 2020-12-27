d1da35f3bd9e811ebc71ebbf94fbcc6ee2d0998e
xrdrx <ryd3rr@gmail.com>
2020-12-27 04:21:31 +0300

    Acronym update

--- acronym/Sources/Acronym/Acronym.swift
+++ acronym/Sources/Acronym/Acronym.swift
@@ -2,15 +2,9 @@
 import Foundation
 
 func abbreviate(_ input: String) -> String {
-    var output = ""
     let nsrange = NSRange(input.startIndex..<input.endIndex, in: input)
     let pattern = #"(?<=^|\W|[a-z])[A-Z]|(?<=\W)[a-z]"#
     let regex = try! NSRegularExpression(pattern: pattern, options: [])
-    regex.enumerateMatches(in: input, options: [], range: nsrange) { (result, flags, stop) in
-        if let result = result {
-            let range = Range(result.range, in: input)!
-            output += input[range]
-        }
-    }
-    return output.uppercased()
+    let matches = regex.matches(in: input, options: [], range: nsrange)
+    return matches.reduce("") { $0 + input[Range($1.range, in: input)!].uppercased() }
 }
